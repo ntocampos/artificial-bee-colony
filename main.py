231557da96f08488233860197358cf1793527b21
Moisés Neto <ntocampos@gmail.com>
2016-06-13 18:40:15 -0300

    ABC done and tested. Showing some errors in classification with misclassifications always in the same class.

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import numpy as np
 import random
+import operator
 
 class Centroid():
     def __init__(self, cl, acc):
@@ -18,7 +19,11 @@ class Centroid():
 
 # Reads and normalize the database, returns the data and classes apart
 def readDatabase(filename):
-    dataset = np.loadtxt('databases\\{filename}'.format(filename = filename), delimiter = ',')
+    filepath = 'databases\\' + filename
+    with open(filepath) as f:
+        # Getting only the lines without missing attribute
+        lines = (line for line in f if '?' not in line)
+        dataset = np.loadtxt(lines, delimiter = ',')
 
     # Shuffing the dataset, once sometimes the data are grouped by class
     np.random.shuffle(dataset)
@@ -32,12 +37,12 @@ def readDatabase(filename):
 
     # Normalizing the data in the [0 1] interval
     arr_max = np.max(dataset, axis = 0) # gets the max of each column
-    arr_min = mp.min(dataset, axis = 0) # gets the min of each column
+    arr_min = np.min(dataset, axis = 0) # gets the min of each column
 
     rows, cols = np.shape(dataset)
     for i in range(rows):
         for j in range(cols):
-            dataset[i][j] = (data[i][j] - arr_min[j]) / (arr_max[j] - arr_min[j])
+            dataset[i][j] = (dataset[i][j] - arr_min[j]) / (arr_max[j] - arr_min[j])
 
     return dataset, classes
 
@@ -65,7 +70,7 @@ def determineCentroids(dataset, classes):
 def euclidianDistance(a, b):    
     diff_sqrt = [(x - y)**2 for x, y in zip(a, b)]
 
-    return np.num(diff_sqrt)
+    return np.sqrt(np.sum(diff_sqrt))
 
 # The sum of the distances between a data point and its class centroid
 # in the trainning set
@@ -75,22 +80,22 @@ def costFunction(dataset, classes, cl, centroid):
     count = 0
     for i, d in enumerate(dataset):
         if str(classes[i]) == cl: # limiting the search only in the specific class
-            distances += euclidianDistance(d, centroids[cl])
+            distances_sum += euclidianDistance(d, centroids[cl])
             count += 1
 
-    return distances / count
+    return distances_sum / count
 
 def fitnessFunction(dataset, classes, centroids):
     fitness = centroids.copy()
     for key in fitness:
-        fitness[key] = 1/(1 + costFunction(dataset, classes, key, centroid[key]))
+        fitness[key] = 1/(1 + costFunction(dataset, classes, key, centroids[key]))
         # TODO: this function can be optimized by storing the costs for each class
 
     return fitness
 
 def rouletteWheelFunction(P):
     p_sorted_asc = sorted(P.items(), key = operator.itemgetter(1))
-    p_sorted_desc = list(reversed(p_sorted_asc))
+    p_sorted_desc = dict(reversed(p_sorted_asc))
 
     pick = np.random.uniform(0, 1)
     current = 0
@@ -102,26 +107,33 @@ def rouletteWheelFunction(P):
 
 
 # Artificial Bee Colony algorithm implementation
-def ABC(dataset, classes, centroids):
+def ABC(dataset, classes, centroids, a_limit, max_iter):
     n_data, n_attr = np.shape(dataset) # Number of cases and number of attributes in each case
     n_bees = len(centroids) # Number of bees in the problem
     var_min = 0 # Minimum possible for each variable
     var_max = 1 # Maximum possible for each variable
-    max_iter = 100 # Maximum number of iterations
-    a_limit = 100 # Abandonment limit
 
     keys = [key for key in centroids] # centroid keys
 
     # Initialize the counter of rejections array
     C = centroids.copy()
-    for key in F:
+    for key in C:
         C[key] = 0
+
+    # Initilize the cost array
+    costs = centroids.copy()
+    for cl in costs:
+        costs[cl] = costFunction(dataset, classes, cl, centroids[cl])
+
+    best_solution = 99999999
+    best_solutions = np.zeros(max_iter)
 
     for it in range(max_iter):
         # Employed bees phase
         for cl in centroids:
             _keys = keys.copy() # copying to maintain the original dict
-            del _keys[cl]
+            index = _keys.index(cl)
+            del _keys[index]
             k = random.choice(_keys) # getting a index k different from i
 
             # Define phi coefficient to generate a new solution
@@ -140,25 +152,27 @@ def ABC(dataset, classes, centroids):
             new_solution_cost = costFunction(dataset, classes, cl, new_solution)
 
             # Greedy selection: comparing the new solution to the old one
-            if new_solution_cost <= costFunction(dataset, classes, cl, centroids[cl]):
+            if new_solution_cost <= costs[cl]:
                 centroids[cl] = new_solution
+                costs[cl] = new_solution_cost
             else: 
                 # Increment the counter for discarted new solutions
                 C[cl] += 1
 
-        F = fitness(dataset, classes, centroids) # calculate fitness of each class
-        f_sum_arr = [val for key, val in F]
+        F = fitnessFunction(dataset, classes, centroids) # calculate fitness of each class
+        f_sum_arr = [F[key] for key in F]
         f_sum = np.sum(f_sum_arr)
         P = {} # probabilities of each class
         for key in F:
             P[key] = F[key]/f_sum
 
-        # Onlooker phase
+        # Onlooker bees phase
         for cl_o in centroids:
             selected_key = rouletteWheelFunction(P)
 
             _keys = keys.copy() # copying to maintain the original dict
-            del _keys[selected_key]
+            index = _keys.index(selected_key)
+            del _keys[index]
             k = random.choice(_keys) # getting a index k different from i
 
             # Define phi coefficient to generate a new solution
@@ -170,22 +184,84 @@ def ABC(dataset, classes, centroids):
             # (centroids[selected_key] - centroids[k]): numpy array
             # The operation will be element by element given that all the operands
             # are numpy arrays
-            new_solution = centroids[selected_key] + phi * (centroids[selected_key] - centroids[k])
             # TODO: ceil and floor of the new solution
+            new_solution = centroids[selected_key] + phi * (centroids[selected_key] - centroids[k])
 
             # Calculate the cost of the dataset with the new centroid
             new_solution_cost = costFunction(dataset, classes, selected_key, new_solution)
 
             # Greedy selection: comparing the new solution to the old one
-            if new_solution_cost <= costFunction(dataset, classes, selected_key, centroids[selected_key]):
-                centroids[cl] = new_solution
+            if new_solution_cost <= costs[selected_key]:
+                centroids[selected_key] = new_solution
+                costs[selected_key] = new_solution_cost
             else: 
                 # Increment the counter for discarted new solutions
-                C[cl] += 1
+                C[selected_key] += 1
+
+        # Scout bees phase
+        for cl_s in centroids:
+            if C[cl_s] > a_limit:
+                random_solution = np.random.uniform(0, 1, n_attr)
+                random_solution_cost = costFunction(dataset, classes, cl_s, random_solution)
+
+                centroids[cl_s] = new_solution
+                costs[cl_s] = random_solution_cost
+                C[cl_s] = 0
+
+        # Update best solution for this iteration
+        best_solution = 9999999999
+        for cl in centroids:
+            if costs[cl] < best_solution:
+                best_solution = costs[cl]
+
+        best_solutions[it] = best_solution
+
+        print('Iteration: {it}; Best cost: {best_solution}'.format(it = "%03d" % it, best_solution = best_solution))
+
+    return best_solutions, centroids
+
+def nearestCentroidClassifier(data, centroids):
+    distances = centroids.copy()
+    for key in centroids:
+        distances[key] = euclidianDistance(data, centroids[key])
+
+    distances_sorted = sorted(distances.items(), key = operator.itemgetter(1))
+    nearest_class, nearest_centroid = distances_sorted[0]
+
+    return nearest_class
+
+def getSets(dataset, classes):
+    size = len(dataset)
+
+    trainning_set = dataset[:round(size * 0.75), :]
+    trainning_set_classes = classes[:round(size * 0.75)]
+
+    test_set = dataset[round(size * 0.75):, :]
+    test_set_classes = classes[round(size * 0.75):]
+
+    return trainning_set, test_set, trainning_set_classes, test_set_classes
 
 
 
+d, c = readDatabase('cancer_int.data')
+trainning_set, test_set, trainning_set_classes, test_set_classes = getSets(d, c)
 
-d, c = readDatabase('glass.data')
-stats, centroids = determineCentroids(d, c)
+stats, centroids = determineCentroids(trainning_set, trainning_set_classes)
+best_soltions, new_centroids = ABC(trainning_set, trainning_set_classes, centroids.copy(), 50, 100)
 
+flag = True
+for cl in centroids:
+    if (centroids[cl] != new_centroids[cl]).all():
+        flag = False
+
+print('Are the centroids equal? ', flag)
+
+count = 0
+for i, val in enumerate(test_set):
+    cl = nearestCentroidClassifier(test_set[i], new_centroids)
+    if cl != str(test_set_classes[i]):
+        print("Miscl.: {data}; Correct: {correct}; Classif.: {classif}"
+            .format(data = test_set[i], correct = test_set_classes[i], classif = cl))
+        count += 1
+
+print('## Errors: {count} Total: {total} ##'.format(count = count, total = len(test_set)))
